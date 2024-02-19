64ed426bb0570ec769c3366d61c1588536257306
VTUY23 <vuuy697777@gmail.com>
2024-02-19 20:35:58 +0700

    alo

---
+++ project12/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $navItems = [
+        "GIỚI THIỆU",
+        "TIN TỨC & THÔNG BÁO",
+        "TUYỂN SINH",
+        "ĐÀO TẠO",
+        "NGHIÊN CỨU",
+        "ĐỐI NGOẠI",
+        "VĂN BẢN",
+        "SINH VIÊN",
+        "LIÊN HỆ"
+    ];
+    echo '<nav><ul>';
+    foreach ($navItems as $item) {
+        echo "<li>$item</li>";
+    }
+    echo '</ul></nav>';
+    ?>
+
+</body>
+
+</html>
